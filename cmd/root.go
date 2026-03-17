77d4ac3cedc2fdccc37c1ef442e3f043ee225158
Jonathan Dieu <jonathanhuydieu@gmail.com>
2026-03-16 22:55:22 -0400

    feat(init): create barebones dieuctl

---
+++ cmd/root.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jonathandieu/dieuctl/cmd/slack"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "dieuctl",
+	Short: "A personal swiss-army knife CLI",
+	Long:  "A personal swiss-army knife CLI for automating the things I'm too lazy to do manually but too stubborn to do differently.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/dieuctl/config.yaml)")
+	rootCmd.AddCommand(slack.Cmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		viper.AddConfigPath(home + "/.config/dieuctl")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jonathandieu/dieuctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
